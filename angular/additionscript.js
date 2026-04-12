9067f91724d7f8c9abff237826b6427c987a2daa
bodigesainath <167r1a05d0@gmail.com>
2026-04-12 09:40:59 -0400

    day 2 session

---
+++ angular/additionscript.js
@@ -0,0 +1,8 @@
+
+        let a=10;
+        let b=20;
+        let sum=a+b;
+        console.log ("addition script coming from external JS file");
+        console.log("The sum of a and b is: " + sum);
+        console.log(` the sum of ${a} and ${b} is ${a+b}`);
+    
